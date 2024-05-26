61ced46952d3d9db096456ae05f08f9e925d30a1
Adrian Astalus <astalus.adrian1703@gmail.com>
2024-05-26 18:20:38 +0300

    Secrets class + artists JSON

--- main.py
+++ main.py
@@ -1,69 +1,84 @@
-import base64
+import os
+import json
 import requests
-import datetime
 from urllib.parse import urlencode
 
-# client_id = 'ba85cdc0478c4a2ab28221b17c89354b'
-# client_secret = 'd101ba0ee42f491c883f9918d62114b8'
-
-# client_creds = f"{client_id}:{client_secret}"
-# client_creds_b64 = base64.b64encode(client_creds.encode()).decode()
-
-# token_url = 'https://accounts.spotify.com/api/token'
-# token_data = {
-#     'grant_type' : 'client_credentials'
-# }
-# token_headers = {
-#     'Authorization' : f'Basic {client_creds_b64}'
-# }
-
-# req = requests.post(token_url, data=token_data, headers=token_headers)
-# token_response_data = req.json()
-# access_token = token_response_data['access_token']
-# # Nice to have: script that each hour refreshes the access token
-# print(access_token)
+from secrets import Secrets
 
 
-
-access_token = 'BQC4DBD9tPaSRWebyEHWkVp72bJvOunqbj-PIs6pSxcNBJFu4XgGOxuQS2dO_dXmYZ3vHuWDTUS2j6hxQODX8RhtReuuwiekwUQeHL5DR0Rv4dTyicI'
-
-headers = {
-    'Authorization':f'Bearer {access_token}'
-}
-
-endpoint = 'https://api.spotify.com/v1/search'
-offset = 0
-artist_id = 1
-
-for i in range(25):
-    data = urlencode({
-    'q' : 'year:2023',
-    'type' : 'artist',
-    'market' : 'US',
-    'limit': '20', 
-    'offset': offset
-    })
-
-    lookup = f'{endpoint}?{data}' # ok
-    print(f"Lookup for loop {i}: {lookup}")
-
-    try:
-
-        r = requests.get(lookup, headers=headers)
-        response_json = r.json()
-        print(f"Next URL: {response_json['artists']['next']}\n\n")
-        artists = response_json['artists']['items']
-        for artist in artists:
-            print(f"{artist_id})  Artist name: {artist['name']}, Artist id: {artist['id']}")
-            artist_id += 1
-
-        offset += 20
-    except:
-        offset += 20
-        continue
+def load_artists(filename):
+    if os.path.exists(filename):
+        with open(filename, 'r') as file:
+            return json.load(file)
+    else:
+        return []
 
 
+def save_artists(artists, filename):
+    with open(filename, 'w') as file:
+        json.dump(artists, file, indent=4)
 
 
+def add_artist(number, name, spotify_id):
+    json_file = 'data/artist_data.json'
+    artists = load_artists(json_file)
+    artist = {
+        'number': number,
+        'name': name,
+        'spotify_id': spotify_id
+    }
+    artists.append(artist)
+    save_artists(artists, json_file)
 
 
+def fetch_500_artists():
+    artists_list = []
+    header = {
+        'Authorization': f'Bearer {secrets.get_access_token()}'
+    }
+
+    endpoint = 'https://api.spotify.com/v1/search'
+    offset = 0
+    artist_id = 1
+
+    for i in range(25):
+        data = urlencode({
+            'q': 'year:2023',
+            'type': 'artist',
+            'market': 'US',
+            'limit': '20',
+            'offset': offset
+        })
+        lookup = f'{endpoint}?{data}'  # ok
+        print(f"Lookup for loop {i}: {lookup}")
+
+        try:
+
+            r = requests.get(lookup, headers=header)
+            print(f"Response: {r.status_code}")
+            response_json = r.json()
+            print(f"Next URL: {response_json['artists']['next']}\n\n")
+            artists = response_json['artists']['items']
+            for artist in artists:
+                print(f"Adding {artist['name']} with ID {artist_id}")
+                artist_details = {
+                    'number': artist_id,
+                    'name': artist['name'],
+                    'spotify_id': artist['id']
+                }
+                artists_list.append(artist_details)
+                artist_id += 1
+
+            offset += 20
+        except:
+            offset += 20
+            continue
+
+    return artists_list
+
+
+if __name__ == '__main__':
+    secrets = Secrets()
+    artists = fetch_500_artists()
+    # print(artists)
+    save_artists(artists, 'data/artist_data.json')
--- secrets.py
+++ secrets.py
@@ -1,2 +1,37 @@
-client_id = 'ba85cdc0478c4a2ab28221b17c89354b'
-client_secret = 'd101ba0ee42f491c883f9918d62114b8'
+import base64
+
+import requests
+from urllib.parse import urlencode
+
+
+class Secrets:
+    def __init__(self):
+        self.__client_id = 'ba85cdc0478c4a2ab28221b17c89354b'
+        self.__client_secret = 'd101ba0ee42f491c883f9918d62114b8'
+
+        self.__client_credentials = f"{self.__client_id}:{self.__client_secret}"
+        self.__client_credentials_b64 = base64.b64encode(self.__client_credentials.encode()).decode()
+
+        self.__token_url = 'https://accounts.spotify.com/api/token'
+        self.__token_data = {
+            'grant_type': 'client_credentials'
+        }
+        self.__token_headers = {
+            'Authorization': f'Basic {self.__client_credentials_b64}'
+        }
+        self.__access_token = self.__fetch_access_token()
+
+    def __fetch_access_token(self):
+        req = requests.post(self.__token_url, data=self.__token_data, headers=self.__token_headers)
+        response_data = req.json()
+        return response_data['access_token']
+
+    def __set_access_token(self):
+        print('Setting access token')
+        self.__access_token = self.__fetch_access_token()
+
+    def refresh_access_token(self):
+        self.__fetch_access_token()
+
+    def get_access_token(self):
+        return self.__access_token
